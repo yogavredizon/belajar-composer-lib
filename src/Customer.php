99020ed3d132cc3e2bc2b023f48e57110dca23f8
Yoga Vredizon <yogavredizon7@gmail.com>
2022-12-08 19:38:41 +0700

    Membuat library

---
+++ src/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vredizon\Customer{
+
+    class Customer {
+        
+        public static function helloCustomer(string $name){
+            return "Hello".$name;
+        }
+    }
+}
